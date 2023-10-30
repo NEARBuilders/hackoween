5b9f8fea9e69a471e125b6410d095c69807de812
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-10-30 16:17:20 -0400

    feat: adds title

--- apps/hackoween/widget/blocks/content.jsx
+++ apps/hackoween/widget/blocks/content.jsx
@@ -10,11 +10,17 @@ const ButtonRow = styled.div`
   margin-bottom: 20px;
 `;
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
 const Button = styled.button`
   background-color: #ed7d31;
   color: #f6f1ee;
   padding: 10px 20px;
-  border: 2px solid #6C5F5B; 
+  border: 2px solid #6c5f5b;
   border-radius: 5px;
   font-size: 16px;
   cursor: pointer;
@@ -43,6 +49,30 @@ const Content = styled.div`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
+const ImageWrapper = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const Name = styled.div`
+  flex: 1;
+  align-self: center;
+  font-size: 2em;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const { selectedItem } = props;
 
 const [activeView, setActiveView] = useState("post");
@@ -65,29 +95,37 @@ if (!selectedItem) {
 
 return (
   <Container>
-    <ButtonRow>
-      <Button
-        onClick={() => setActiveView("post")}
-        className={activeView === "post" ? "active" : ""}
-      >
-        Post
-      </Button>
-      <Button
-        onClick={() => setActiveView("demo")}
-        className={activeView === "demo" ? "active" : ""}
-      >
-        Demo
-      </Button>
-      <Button
-        onClick={() => setActiveView("data")}
-        className={activeView === "data" ? "active" : ""}
-      >
-        Data
-      </Button>
-    </ButtonRow>
+    <Header>
+      <Logo>
+        <ImageWrapper>
+          <Image src={selectedItem.metadata.image.href} alt={selectedItem.metadata.name} />
+        </ImageWrapper>
+        <Name>{selectedItem.metadata.name}</Name>
+      </Logo>
+      <ButtonRow>
+        <Button
+          onClick={() => setActiveView("post")}
+          className={activeView === "post" ? "active" : ""}
+        >
+          Post
+        </Button>
+        <Button
+          onClick={() => setActiveView("demo")}
+          className={activeView === "demo" ? "active" : ""}
+        >
+          Demo
+        </Button>
+        <Button
+          onClick={() => setActiveView("data")}
+          className={activeView === "data" ? "active" : ""}
+        >
+          Data
+        </Button>
+      </ButtonRow>
+    </Header>
     <Content>
       {activeView === "post" && (
-        <PostContent data={JSON.parse(selectedItem.data).post } />
+        <PostContent data={JSON.parse(selectedItem.data).post} />
       )}
       {activeView === "demo" && (
         <Widget src={JSON.parse(selectedItem.data).demo} />
